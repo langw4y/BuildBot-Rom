74b732d7d956ff410fffb56a9a60bbb6fcafe596
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-12-22 19:53:08 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -16,7 +16,7 @@ git clone https://github.com/R9Lab/MTKSepolicyVendor.git --depth 1 -b lineage-20
 source build/envsetup.sh
 lunch lineage_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+mka bacon -j10
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
