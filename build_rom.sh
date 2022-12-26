c8c9b8cadaf2786bd7768f009c41c2d2c6cca91c
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-12-27 04:49:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-repo sync --force-sync --no-clone-bundle --current-branch --no-tags -j$(nproc --all)
-git clone https://github.com/kitw4y/device-new --depth 1 -b cipher device/xiaomi/lancelot
+repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b t13.0 -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
+git clone https://github.com/kitw4y/device-new --depth 1 -b aicp device/xiaomi/lancelot
 git clone https://github.com/kitw4y/android_device_xiaomi_mt6768-common --depth 1 -b lineage-20 device/xiaomi/mt6768-common 
 git clone https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 --depth 1 -b  lineage-20 kernel/xiaomi/mt6768
 git clone https://github.com/kitw4y/vendor_xiaomi --depth 1 -b TwistedScarlett vendor/xiaomi
@@ -14,10 +14,10 @@ git clone https://github.com/kitw4y/android_device_mediatek_sepolicy_vnd --depth
 
 # build rom 1
 source build/envsetup.sh
-lunch cipher_lancelot-userdebug
+lunch aicp_lancelot-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-mka bacon -j
+brunch
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
