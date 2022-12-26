7e01be303fdd5a5a4ebbaf0973696042829d15d4
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-12-26 18:11:35 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,18 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b evox13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom 1
 source build/envsetup.sh
 lunch evolution_lancelot-userdebug
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
-ccache -M 20G
-ccache -o compression=true
-ccache -z 
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka
-mka evolution -j
+export TZ=Asia/Kolkata #put before last build command
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
