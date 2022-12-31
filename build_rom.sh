025341615931b317142b9bf4cdc194f7a64221e2
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-12-31 12:27:16 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,8 +4,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 git clone https://github.com/kitw4y/device-new --depth 1 -b los device/xiaomi/lancelot
 git clone https://github.com/kitw4y/android_device_xiaomi_mt6768-common --depth 1 -b lineage-20 device/xiaomi/mt6768-common 
 git clone https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 --depth 1 -b  lineage-20 kernel/xiaomi/mt6768
-git clone https://github.com/kitw4y/vendor_xiaomi --depth 1 -b TwistedScarlett vendor/xiaomi
-git clone https://github.com/kitw4y/android_hardware_mediatek --depth 1 -b lineage-20 hardware/mediatek
+git clone https://github.com/galang8664/vendor_xiaomi_lancelot.git vendor/xiaomi/lancelot
+git clone https://github.com/galang8664/common-vendor.git vendor/xiaomi/mt6768-common
+git clone https://github.com/xiaomi-mt6785-dev/android_hardware_mediatek.git hardware/mediatek
 git clone https://github.com/kitw4y/ImsService --depth 1 -b lineage-20 packages/app/ImsService
 git clone https://github.com/kitw4y/android_packages_apps_MtkFMRadio --depth 1 -b lineage-20 packages/apps/MtkFMRadio
 git clone https://github.com/kitw4y/vendor_mediatek_opensource_interfaces --depth 1 -b lineage-20 vendor_mediatek_opensource_interfaces
