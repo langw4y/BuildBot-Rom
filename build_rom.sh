eb2c7b7bc4fc496dcbfcb063940bc809e5c12490
galang8664 <95033122+galang8664@users.noreply.github.com>
2023-01-01 14:49:53 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
-git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b bliss .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Evolution-X-tiramisu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-repo sync -j1 --fail-fast
 
-# build rom 
+# build rom
 source build/envsetup.sh
-lunch bliss_lancelot-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Kolkata #put before last build command
-blissify -g lancelot
+lunch evolution_mido-userdebug
+export KBUILD_BUILD_USER=Lang
+export KBUILD_BUILD_HOST=17
+export BUILD_USERNAME=Lang
+export BUILD_HOSTNAME=17
+export TZ=Asia/Delhi #put before last build command
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
