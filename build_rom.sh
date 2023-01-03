a066a81a1f84ebfd223d7d19dc0b3e1281d0ba27
galang8664 <95033122+galang8664@users.noreply.github.com>
2023-01-04 05:26:51 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -15,3 +15,17 @@ mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# sync rom
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/StatiXOS/android_manifest.git -b tm-qpr1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b statix .repo/local_manifests
+repo sync --force-sync --no-clone-bundle --current-branch --no-tags -j$(nproc --all)
+
+# build rom 
+source build/envsetup.sh
+lunch statix_lancelot-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Kolkata #put before last build command
+brunch statix_lancelot-userdebug
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
