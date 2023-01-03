3238a91003ddd245ed2f0989d9520602a19c253f
galang8664 <95033122+galang8664@users.noreply.github.com>
2023-01-04 05:27:24 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,21 +1,4 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Evolution-X-tiramisu .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# build rom
-source build/envsetup.sh
-lunch evolution_mido-userdebug
-export KBUILD_BUILD_USER=Lang
-export KBUILD_BUILD_HOST=17
-export BUILD_USERNAME=Lang
-export BUILD_HOSTNAME=17
-export TZ=Asia/Delhi #put before last build command
-mka evolution
-
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-# sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/StatiXOS/android_manifest.git -b tm-qpr1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b statix .repo/local_manifests
 repo sync --force-sync --no-clone-bundle --current-branch --no-tags -j$(nproc --all)
