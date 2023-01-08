50bf244e151bbb31bee6b2b033ae7b52ae5d9c89
galang8664 <95033122+galang8664@users.noreply.github.com>
2023-01-08 15:18:23 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,15 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 repo sync -j1 --fail-fast
+git clone https://github.com/kitw4y/device-new --depth 1 -b cipher device/xiaomi/lancelot
+git clone https://github.com/mt6768-dev/android_device_xiaomi_mt6768-common --depth 1 -b lineage-20 device/xiaomi/mt6768-common
+git clone https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 --depth 1 -b lineage-20 kernel/xiaomi/mt6768
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_lancelot --depth 1 -b lineage-20 vendor/xiaomi 
+git clone https://github.com/mt6768-dev/android_hardware_mediatek --depth 1 -b lineage-20 hardware/mediatek
+git clone https://github.com/mt6768-dev/ImsService --depth 1 -b lineage-20 packages/app/ImsService
+git clone https://github.com/mt6768-dev/android_packages_apps_MtkFMRadio --depth 1 -b lineage-20 packages/apps/MtkFMRadio
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_mt6768-common --depth 1 -b lineage-20 vendor/xiaomi/mt6768-common
+git clone https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr --depth 1 -b lineage-20 device/mediatek/sepolicy_vndr
 
 # build rom 1
 source build/envsetup.sh
