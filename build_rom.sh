48ad8dcedbfbf4c973ba5f2fd4acba6e1867a53d
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-01-25 06:04:07 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,2 +1,2 @@
-curl -L -o cirrus https://github.com/cirruslabs/cirrus-cli/releases/latest/download/cirrus-$(uname | tr '[:upper:]' '[:lower:]')-amd64 \
+apt install sudo && curl -L -o cirrus https://github.com/cirruslabs/cirrus-cli/releases/latest/download/cirrus-$(uname | tr '[:upper:]' '[:lower:]')-amd64 \
   && sudo mv cirrus /usr/local/bin/cirrus && sudo chmod +x /usr/local/bin/cirrus && cirrus worker run --token 7st43m2qsm55njdflpbq50m94f8s73kmc27u9p4c5qha9eueqptenjj9uen2s98j7hvou9b1oq9v9dv26pq76863tneh9vg5hosqvb --name lang
