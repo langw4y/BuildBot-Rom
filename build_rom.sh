ab48e01e62c3ef25793efbaabcd3ebcab9a32279
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-01-26 03:55:19 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,2 +1,13 @@
-curl -L -o cirrus https://github.com/cirruslabs/cirrus-cli/releases/latest/download/cirrus-$(uname | tr '[:upper:]' '[:lower:]')-amd64 \
-  && mv cirrus /usr/local/bin/cirrus && chmod +x /usr/local/bin/cirrus && cirrus worker run --token 7st43m2qsm55njdflpbq50m94f8s73kmc27u9p4c5qha9eueqptenjj9uen2s98j7hvou9b1oq9v9dv26pq76863tneh9vg5hosqvb --name lang
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b evox .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom 
+source build/envsetup.sh
+lunch evolution_lancelot-userdebug
+export TZ=Asia/Jakarta #put before last build command
+mka evolution
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
